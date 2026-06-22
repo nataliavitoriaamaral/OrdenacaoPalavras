650b045047df44cd97654cfb2eeb5940aa0c8062
nataliavitoriaamaral <natalia.amaraldoval@gmail.com>
2026-06-22 17:37:00 -0300

    feat: cria classe principal e lista de palavras para o teste

---
+++ Teste.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Teste {
+    public static void main(String[] args) {
+        String[] palavras = {"abacaxi", "maçã", "uvaia", "margarina", "abobrinha", "batom"};
+
+        System.out.println("Lista original:");
+        System.out.println(Arrays.toString(palavras));
+    }
+}
