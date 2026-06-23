f45a4b3884a6a9b9cc1d8705de8247173c3ff00d
nataliavitoriaamaral <natalia.amaraldoval@gmail.com>
2026-06-22 22:15:49 -0300

    feat:aplica o metodo de ordenacao pela ultima letra

--- Teste.java
+++ Teste.java
@@ -16,5 +16,9 @@ public class Teste {
         Arrays.sort(palavras, Comparator.comparingInt(String::length));
         System.out.println("Ordem por tamanho da palavra:");
         System.out.println(Arrays.toString(palavras));
+        // Ordem pela ultima letra usando o comparator customizado
+        Arrays.sort(palavras, new ComparadorUltimaLetra());
+        System.out.println("Ordem pela ultima letra:");
+        System.out.println(Arrays.toString(palavras));
     }
 }
